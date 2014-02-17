222f54a4be1f7e5cfd3f38d4c815e200e41f5b8d
bryan kennedy <bkennedy@smm.org>
2014-02-17 16:27:02 -0600

    Adding some badic conditionals
    Also improving the feedback text

--- boxen-setup.sh
+++ boxen-setup.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-echo <<EOF
-Installing Boxen
+echo "Installing Boxen"
+echo "You will be prompted for passwords. The first request will be for"
+echo "your sudo password on the computer you are setting up."
+echo ""
+echo "The Boxen script will prompt you for your GitHub credentials. You should"
+echo "use the SMM Machines account."
 
-You will be prompted for passwords. The first request will be for
-your sudo password on the computer you are setting up.
+if [ ! -d /opt/boxen/repo ]; then
+  sudo mkdir -p /opt/boxen/repo
+fi
 
-The Boxen script will prompt you for your GitHub credentials. You should
-use the SMM Machines account.
-EOF
-
-sudo mkdir -p /opt/boxen/repo
 sudo chown -R ${USER}:staff /opt
-git clone https://github.com/scimusmn/boxen.git /opt/boxen/repo
+
+if [ ! -d /opt/boxen/repo/.git ]; then
+  git clone https://github.com/scimusmn/boxen.git /opt/boxen/repo
+fi
+
 cd /opt/boxen/repo
+
 ./script/boxen --no-fde --debug
