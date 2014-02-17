517f093fecec0235424501f22efa1adb6326a0c7
bryan kennedy <bkennedy@smm.org>
2014-02-17 16:21:57 -0600

    Adding some feedback to the script

--- boxen-setup.sh
+++ boxen-setup.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+echo <<EOF
+Installing Boxen
+
+You will be prompted for passwords. The first request will be for
+your sudo password on the computer you are setting up.
+
+The Boxen script will prompt you for your GitHub credentials. You should
+use the SMM Machines account.
+EOF
+
 sudo mkdir -p /opt/boxen/repo
 sudo chown -R ${USER}:staff /opt
 git clone https://github.com/scimusmn/boxen.git /opt/boxen/repo
