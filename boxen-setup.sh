072acfd39c110b48b5ed8aac73cc444835a3bbde
bryan kennedy <bkennedy@smm.org>
2014-03-28 09:48:50 -0500

    Changing the install script to fix Xcode issue
    See the comment block for explanation.

--- boxen-setup.sh
+++ boxen-setup.sh
@@ -19,4 +19,19 @@ fi
 
 cd /opt/boxen/repo
 
-./script/boxen --no-fde --debug
+#
+# The Apple LLVM compiler in Xcode 5.1 treats unrecognized command-line options
+# as errors. This issue has been seen when building both Python native
+# extensions and Ruby Gems, where some invalid compiler options are currently
+# specified.
+#
+# Projects using invalid compiler options will need to be changed to remove those
+# options. To help ease that transition, the compiler will temporarily accept an
+# option to downgrade the error to a warning.
+#
+# This calls that option so that when Boxen tries to compile those various
+# libraries, it won't error out. This seems to be a temporary issue until the
+# various libraries update. I don't think Boxen is going to do anything about
+# it right now. I'm adding it here so that we get a default install behavior.
+#
+ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future ./script/boxen --no-fde --debug
